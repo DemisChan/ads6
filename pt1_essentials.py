ee8de7d36653c120635836f05e6977698e3688a0
Demis Lavrentidis <demislav@hotmail.com>
2019-06-21 09:58:05 +0000

    Update

--- pt1_essentials.py
+++ pt1_essentials.py
@@ -34,7 +34,7 @@ def first_and_last_investigation():
     :rtype: str
     """
     
-    N_o = 'SELECT MIN(application_date), MAX(application_date) FROM businesses'
+    N_o = 'SELECT MIN(RTRIM(LTRIM(REPLACE(application_date, '-', '')))), MAX(RTRIM(LTRIM(REPLACE(application_date, '-', '')))) FROM businesses'
 
     return N_o
 
