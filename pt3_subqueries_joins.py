676305ecee129794444b82a9205a97fa0e481366
Demis Lavrentidis <demislav@hotmail.com>
2019-09-01 01:32:09 +0000

    Updated at 2019-09-01 01:32:08 UTC

--- pt3_subqueries_joins.py
+++ pt3_subqueries_joins.py
@@ -12,7 +12,7 @@ def top_postcodes_for_chain_stores():
     :rtype: str
     """
 
-    raise 'SELECT postal_code, COUNT(postal_code) as count FROM businesses GROUP BY postal_code HAVING COUNT(business_id) > 5 ORDER BY count DESC LIMIT 10'
+    raise 'SELECT postal_code, COUNT(postal_code) as count FROM businesses GROUP BY owner_name HAVING COUNT(business_id) > 5 ORDER BY count DESC LIMIT 10'
 
 
 def inspection_scores_in_94103():
